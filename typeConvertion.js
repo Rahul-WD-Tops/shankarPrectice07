0813a61d0c319240d224db25b4bfd8731522a1c4
Rahul-WD-Tops <140709569+Rahul-WD-Tops@users.noreply.github.com>
2024-08-16 00:10:46 +0530

    js condition

---
+++ typeConvertion.js
@@ -0,0 +1,46 @@
+// Type convertion in js
+
+
+
+
+const a = "10";
+const b = 5;
+const c = a + b
+
+console.log(c);
+// .............................
+
+const d = 20;
+const e = 10;
+const f = d - e
+
+console.log(f)
+//................................
+const g = 100;
+const h = 500;
+const i = g * h
+
+console.log(i);
+
+//....................................
+
+//jb koi velyu string me ho to jodna pde to to jodta nhi h balki conkat ho jata h yani pass me aa jata h
+const x = "10rahul"
+const y = 10;
+const z = x + y;
+// 10rahul10 es typ mepass aa jata h
+console.log(z);
+
+
+const m = 10;
+const n = 15;
+const o = m / n;
+console.log(o);
+
+
+const p = "10r";
+const q = "20 ";
+const r = p + q;
+console.log(r);
+
+
